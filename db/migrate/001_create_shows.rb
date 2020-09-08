125605178cca12e9297bbc15e7c8ba1a81d711fe
Hannah Angle <hannahcangle@gmail.com>
2020-09-08 18:27:17 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,10 +1,10 @@
 class CreateShows < ActiveRecord::Migration[5.2]
-  def change 
+  def change
     create_table :shows do |t|
-      t.string :name 
-      t.sting :network 
-      t.string :day 
-      t.integer :rating 
-    end 
-  end 
-end 
+      t.string :name
+      t.string :day
+      t.string :network
+      t.integer :rating
+    end
+  end
+end
